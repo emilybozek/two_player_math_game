d1e70dad1475157579577b69501d182b2eace25f
Emily Bozek <emily.bozek@gmail.com>
2022-05-02 22:51:14 -0700

    planning

--- example.rb
+++ example.rb
@@ -4,6 +4,15 @@ def game
 
   loop do 
     user_answer = gets.chomp.to_i
-
+    if user_answer == num
+      puts "Yay, you got it right!"
+      break
+    elsif user_answer > num
+      puts "You guessed too high, try guessing lower"
+    elsif user_answer < num
+      puts "You guessed too low, try guessing higher"
+    end
   end
 end
+
+game()
